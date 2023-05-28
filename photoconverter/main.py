5b883aac4dc9a5d0e73681e84def6dd02d261d2e
mountain07 <kxd272@case.edu>
2023-05-28 16:25:54 -0400

    Skip processed pictures

--- photoconverter/main.py
+++ photoconverter/main.py
@@ -10,23 +10,29 @@ def main():
     start = time.time()
 
     # Sometimes the file ending can be .nef or .NEF, therefore I included both possibilities to save extra work.
-    pathnef = "/path/to/*.nef"
-    pathNEF = "/path/to/*.NEF"
+    in_path = "E:/Full Sized Photo Storage/Camera NEF/"
+    path_NEF = in_path+"*.NEF"
+    out_path = "E:/Full Sized Photo Storage/Camera JPG/"
+    path_JSON = out_path +"*.jpg"
     count = 0
-    for path in glob.glob(pathnef):
-        with rawpy.imread(path) as raw:
-            rgb = raw.postprocess()
 
-            imageio.imwrite(path.replace('.nef', '') + '.jpg', rgb)
-            count = count + 1
-            print('image #', count, ' done')
+    # Don't process the pictures that already have been processed
+    in_pics = glob.glob(path_NEF)
+    out_pics = glob.glob(path_JSON)
+    
+    sample = in_pics[0]
+    in_names = [getNameFromPath(path) for path in in_pics]
+    out_names = [getNameFromPath(path) for path in out_pics]
+    unconverted_names = list(set(in_names) - set(out_names))
 
-    for path in glob.glob(pathNEF):
+    
+    for name in unconverted_names:
+        path=in_path+name+".NEF"
         count = +1
 
         with rawpy.imread(path) as raw:
             rgb = raw.postprocess()
-            imageio.imwrite(path.replace('.NEF', '') + '.jpg', rgb)
+            imageio.imwrite(path.replace('.NEF', '.jpg'), rgb)
             count = count + 1
             print('image #', count, ' done')
 
@@ -37,6 +43,9 @@ def main():
 
     print("Elapsed Time:", round(minutes), "minutes and ", round(seconds), "seconds")
 
+def getNameFromPath(path:str = ""):
+    name = path.split("\\")[-1].split(".")[0]
+    return name
 
 if __name__ == '__main__':
     main()
